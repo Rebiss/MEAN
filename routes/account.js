9ba28cba2c821dbac59a7f3ada45a1764caae79f
Rebiss <andranik.barseghyan@betconstruct.com>
2020-01-07 23:25:35 +0400

    change route

--- routes/account.js
+++ routes/account.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
 
-router.get('/reg', (req,res) => { res.send('Registration') });
+router.get('/reg', (req,res) => { 
+    res.send('Registration');
+});
 
-router.get('/auth', (req,res) => { res.send('Authirisation') });
+router.get('/auth', (req,res) => { 
+    res.send('Authirisation');
+});
 
-router.get('/dashboard', (req,res) => { res.send('Dashboard') });
+router.get('/dashboard', (req,res) => { 
+    res.send('Dashboard');
+ });
 
 module.exports = router;
